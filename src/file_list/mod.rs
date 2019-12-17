31da89a03880ef8e52ef14b16d1ab29f9b08fd56
Chili-Bebber <cheezthrozer@gmail.com>
2019-12-17 07:29:35 -0500

    fixed my use of AsRef trait

--- src/file_list/mod.rs
+++ src/file_list/mod.rs
@@ -1,5 +1,9 @@
 use std::path::PathBuf;
+use std::path::Path;
 use std::ffi::OsStr;
+use std::convert::AsRef;
+
+const HOME_PREFIX:&str = "~";
 
 pub struct FileList {
     files:Vec<PathBuf>
@@ -18,10 +22,37 @@ impl FileList {
     where P:AsRef<OsStr> {
         let mut path_list:Vec<PathBuf> = vec!();
         for path in input_list.iter() {
+            let path = path.as_ref();
             path_list.push(PathBuf::from(path));
         }
         FileList {
             files:path_list
+        }
+    }
+    // replace instances of home dir with "~"
+    pub fn use_placeholder_home_dir(&mut self) {
+        self.replace_prefix(home_dir(), HOME_PREFIX);
+    }
+    // replace instances of "~" with home dir
+    pub fn use_real_home_dir(&mut self) {
+        self.replace_prefix(HOME_PREFIX, home_dir());
+    }
+    // replace the beginning of a path with something else
+    fn replace_prefix<A, B>(&mut self, replace:A, with:B)
+    where A:AsRef<Path>,
+          B:AsRef<Path> {
+        let replace = replace.as_ref();
+        let with = with.as_ref();
+        for(index, file_path) in self.files.clone().iter().enumerate() {
+            if file_path.starts_with(&replace) {
+                let mut path_components = file_path.components();
+                path_components.next();
+                if file_name(replace) != HOME_PREFIX {
+                    path_components.next();
+                }
+                let final_path = with.join(path_components);
+                self.files[index] = final_path;
+            }
         }
     }
     // return a Vec containing the names of the files
@@ -31,21 +62,32 @@ impl FileList {
     pub fn file_names(&self) -> Vec<String> {
         let mut file_names:Vec<String> = vec!();
         for file in self.files.iter() {
-            let file_name = match file.file_name() {
-                Some(os_str) => {
-                    match os_str.to_str() {
-                        Some(str_ref) => {
-                            String::from(str_ref)
-                        },
-                        None => panic!("couldn't change OsStr to &str \
-                                       for file: {}", file.display()),
-                    }
-                },
-                None => panic!("file_name() failed for {}", 
-                               file.display()),
-            };
+            let file_name = file_name(file);
             file_names.push(file_name);
         }
         file_names
+    }
+}
+// return the name of a file and panic if it can't be done
+fn file_name<P>(path:P) -> String
+where P:AsRef<Path> {
+    let path = path.as_ref();
+    match path.file_name() {
+        Some(os_str) => {
+            match os_str.to_str() {
+                Some(str_ref) => String::from(str_ref),
+                None => panic!("Couldn't convert OsStr to &str")
+            }
+        },
+        None => panic!("file_name() call failed")
+    }
+}
+
+// helper method to return home directory and panic
+// if it can't be accessed for whatever reason
+fn home_dir() -> PathBuf {
+    match dirs::home_dir() {
+        Some(dir) => dir,
+        None => panic!("Couldn't access home dir")
     }
 }
